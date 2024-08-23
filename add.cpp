80c76a2bf833c4da895c9bbf6a47fa5dbecd8ac6
crisxian-reyes <crisxian.reyes@upr.edu>
2024-08-23 16:32:09 +0000

    Final

---
+++ add.cpp
@@ -0,0 +1,15 @@
+// Crisxian Gabriel Reyes Colon -- crisxian-reyes
+// This program adds two numbers
+
+#include <iostream>
+
+// I try to run the code by compiling it but there doesn't appear a file tha's called "add"
+
+int main() {
+  int number1, number2, sum;
+  number1 = 12;
+  number2 = 13;
+  sum = number1 + number2;
+  std::cout << "The sum of these two integers is " << sum << std::endl;
+  return 0;
+}
---
+++ lab01.cpp
@@ -0,0 +1,18 @@
+
+// Crisxian Gabriel Reyes Colon
+// A program to practice modifications and compilation
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello Solar System!" << std::endl;
+  std::cout << "Hello Galaxy!" << std::endl;
+  std::cout << "Hello Universe!" << std::endl;
+  std::cout << "Hello ... ?" << std::endl;
+  
+  for (int i = 1; i <= 5; i++) {
+    std::cout << "This is a " << i << "!" << std::endl;
+  }
+  return 0;
+}
